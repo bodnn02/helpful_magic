a6f6f5f2ba71b97aba77fe2c6a437114847a7ee4
Богдан Гаскевич <bodnn03@gmail.com>
2022-11-17 02:11:11 +0200

    modified:   article.html 	modified:   articles.html 	modified:   consultation-order.html 	modified:   contacts.html 	modified:   css/style.css 	modified:   forum-article.html 	modified:   forum.html 	modified:   index.html 	modified:   js/main.js 	modified:   master-page.html 	modified:   search-extended.html 	modified:   search.html 	modified:   services.html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(".articles-list_scroll__before-btn").click(function () {
     let gallery = $(this).parent();
     if(gallery.attr("current-page") == "0")
     {
-        gallery.attr("current-page", gallery.children("li").length);
+        gallery.children("articles-list_scroll__before-btn").show();
+        gallery.children("articles-list_scroll__next-btn").hide();
+        gallery.attr("current-page", parseInt(gallery.children("li").length)/3);
         gallery.css("margin-left", (parseInt(gallery.attr("current-page"))*100*(-1) + "%"));
     } else {
         gallery.attr("current-page",parseInt(gallery.attr("current-page"))-1);
@@ -11,8 +13,10 @@ $(".articles-list_scroll__before-btn").click(function () {
 });
 $(".articles-list_scroll__next-btn").click(function () {
     let gallery = $(this).parent();
-    if(gallery.attr("current-page") == gallery.children("li").length)
+    if(gallery.attr("current-page") >= (parseInt(gallery.children("li").length)/3) - 1 )
     {
+        gallery.children("articles-list_scroll__before-btn").hide();
+        gallery.children("articles-list_scroll__next-btn").show();
         gallery.attr("current-page","0");
         gallery.css("margin-left", (parseInt(gallery.attr("current-page"))*100*(-1) + "%"));
     } else {
